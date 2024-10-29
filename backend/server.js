d35e14c049690a1ac8b63694da7e444101ab3dcd
Muhidin3 <muhidinshemsu3@gmail.com>
2024-10-29 17:13:49 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(cors())
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-app.use(express.static(path.join(__dirname,'../front-end/dist')))
+app.use(express.static(path.join(__dirname,'/front-end/dist')))
 
 
 
